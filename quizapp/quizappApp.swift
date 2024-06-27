6532bc77881b59d7fbcce4309a11d0a30389d8c8
khashika narain <khashika.narain@gmail.com>
2024-06-26 22:38:58 -0500

    Initial Commit

---
+++ quizapp/quizappApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizappApp.swift
+//  quizapp
+//
+//  Created by Sruti Narain on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct quizappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
